f97f1f322574d2c4ed41d4f5e97ac0a85993afc7
DuongWuangDat <duongquangdat172004@gmail.com>
2024-04-26 13:06:40 +0700

    add message

--- app.js
+++ app.js
@@ -22,12 +22,40 @@ const io = new Server(http, {
         origin: "*"
     }
 })
-
+let onlineUsers = []
 io.on("connection", (socket)=>{
     console.log(`${socket.id} connected`)
 
     //**Code socket's event listener here */
-
+    socket.on("addOnlineUser", (userID)=>{
+        const check = onlineUsers?.some((user) => user.userID == userID) 
+        if(check){
+         const index = onlineUsers.findIndex((user) => user.userID == userID)
+         onlineUsers[index].socketID = socket.id
+        }else{
+         onlineUsers.push({
+             userID: userID,
+             socketID: socket.id
+         })
+        }
+         
+         console.log(onlineUsers)
+         io.emit("getOnlineUsers", onlineUsers)
+     })
+     
+     socket.on("sendMessage", (req)=>{
+         console.log(req.recipientID)
+         const user = onlineUsers.find((userFind) => userFind.userID == req.recipientID)
+         console.log(user)
+         if(user){
+             io.to(user.socketID).emit("getMessage", req)
+         }
+     })
+    
+    socket.on('disconnect', () => {
+        onlineUsers = onlineUsers.filter((user)=> user.userID !== userID)
+        io.emit("getOnlineUsers", onlineUsers)
+    });
 
     //**------------------------------------- */
 
